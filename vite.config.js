fc9c4b97d657a38dc6573a52e39321e45cd47520
Alex Witter <alex@Alexs-MBP.localdomain>
2025-01-29 16:43:45 -0500

    first-commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  base: "./", // Ensures relative paths in the built HTML
+  plugins: [
+    injectHTML(),
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ommig: resolve(__dirname, 'om-mig.html'),
+        nav: resolve(__dirname, 'partials/nav.html'),
+        footer: resolve(__dirname, 'partials/footer.html'),
+      },
+    },
+  },
+});
